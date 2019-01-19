640e22be2964bd3777852ecc608d339b70e24630
yantingji <yantingji@126.com>
2019-01-19 14:33:46 +0900

    新追加

--- code.autogeneration/src/main/java/com/itedu365/code/autogeneration/MyMojo.java
+++ code.autogeneration/src/main/java/com/itedu365/code/autogeneration/MyMojo.java
@@ -21,17 +21,15 @@ import org.apache.maven.plugin.MojoExecutionException;
 
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
-import org.apache.maven.plugins.annotations.Parameter;
-import org.apache.maven.plugins.annotations.ResolutionScope;
 
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
 /**
- * Goal which touches a timestamp file.
  *
- * @deprecated Don't use!
+ *@author 365ITedu
+ *@version=0.0.1
  */
 @Mojo( name = "touch", defaultPhase = LifecyclePhase.PROCESS_SOURCES )
 public class MyMojo
@@ -40,7 +38,6 @@ public class MyMojo
     /**
      * Location of the file.
      */
-    @Parameter( defaultValue = "${project.build.directory}", property = "outputDir", required = true )
     private File outputDirectory;
 
     public void execute()

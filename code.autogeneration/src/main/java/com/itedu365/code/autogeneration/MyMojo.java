fa6d3d06b4efce673aa01bd89551a96725016262
yantingji <yantingji@126.com>
2019-01-20 11:37:16 +0900

    完善

--- code.autogeneration/src/main/java/com/itedu365/code/autogeneration/MyMojo.java
+++ code.autogeneration/src/main/java/com/itedu365/code/autogeneration/MyMojo.java
@@ -16,66 +16,14 @@ package com.itedu365.code.autogeneration;
  * limitations under the License.
  */
 
-import org.apache.maven.plugin.AbstractMojo;
-import org.apache.maven.plugin.MojoExecutionException;
-
-import org.apache.maven.plugins.annotations.LifecyclePhase;
-import org.apache.maven.plugins.annotations.Mojo;
-
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
 
 /**
  *
  *@author 365ITedu
- *@version=0.0.1
+ *@version 0.0.1
  */
-@Mojo( name = "touch", defaultPhase = LifecyclePhase.PROCESS_SOURCES )
 public class MyMojo
-    extends AbstractMojo
 {
-    /**
-     * Location of the file.
-     */
-    private File outputDirectory;
 
-    public void execute()
-        throws MojoExecutionException
-    {
-        File f = outputDirectory;
 
-        if ( !f.exists() )
-        {
-            f.mkdirs();
-        }
-
-        File touch = new File( f, "touch.txt" );
-
-        FileWriter w = null;
-        try
-        {
-            w = new FileWriter( touch );
-
-            w.write( "touch.txt" );
-        }
-        catch ( IOException e )
-        {
-            throw new MojoExecutionException( "Error creating file " + touch, e );
-        }
-        finally
-        {
-            if ( w != null )
-            {
-                try
-                {
-                    w.close();
-                }
-                catch ( IOException e )
-                {
-                    // ignore
-                }
-            }
-        }
-    }
 }
